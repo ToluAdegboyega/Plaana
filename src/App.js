3e89d5f2695ebbda473a611c0e49ec300273bcd3
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2020-09-19 13:12:42 +0100

    index.js page cleanup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Container from '@material-ui/core/Container';
 
 function App() {
   return (
+    <Container>
     <h1> In Progress... </h1>
+    </Container>
   );
 }
 
