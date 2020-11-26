6eae3d9c6954980192f4d30def91d5d791d09298
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2020-11-26 13:30:06 +0100

    chore: add route components.

--- src/App.js
+++ src/App.js
@@ -2,24 +2,34 @@ import React from "react";
 import "./assets/App.css";
 import Box from "@material-ui/core/Box";
 import { makeStyles } from '@material-ui/core/styles';
-import TodoList from "./components/TodoList";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import TodoList from './components/TodoList';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import  PrivateRoute  from  './PrivateRoute';
+import  {AuthProvider}  from  './Authentication';
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    paddingTop: '180px',
+    paddingTop: '100px',
     display: 'flex',
     justifyContent: 'center'
   }
 }));
 
 function App() {
-
   const classes = useStyles();
-
   return (
-    <Box className={classes.container}>
-      <TodoList/>
-    </Box>
+    <AuthProvider>
+      <Router>
+        <Box className={classes.container} >
+          <PrivateRoute exact path="/Todo-List" component={TodoList} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+        </Box>
+      </Router>
+    </AuthProvider>
+    
   );
 }
 
